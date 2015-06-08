75e3f10fe743dd1de8795e3c9a1105cb4a751224
McKenneth Scott <mckscott@McKenneths-MacBook-Air.local>
2015-06-08 15:24:27 -0400

    updated gems, editing specs

--- spec/controllers/actions_controller_spec.rb
+++ spec/controllers/actions_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Api::ActionsController, type: :controller do
   describe 'GET #index' do
     before :each do
       @actions = [Action.create!, Action.create!, Action.create!]
+      request.env['HTTP_ACCEPT'] = 'application/json'
     end
     it 'responds successfully with an HTTP 200 status code' do
       get :index
@@ -14,15 +15,19 @@ RSpec.describe Api::ActionsController, type: :controller do
       get :index
       expect(assigns(:actions)).to match_array(@actions)
     end
-    it 'responds to requests for json' do
-      request.env['HTTP_ACCEPT'] = 'application/json'
-      get :index
-      expect(response.body).to eq(@actions.to_json)
+    context 'a request is made for json data' do
+      it 'responds to requests for json' do
+        get :index
+        expect(response.body).to eq(@actions.to_json)
+      end
     end
-    it 'responds to request for xml' do
-      request.env['HTTP_ACCEPT'] = 'application/xml'
-      get :index
-      expect(response.body).to eq(@actions.to_xml)
+
+    context 'a request is made for xml data' do
+      before { request.env['HTTP_ACCEPT'] = 'application/xml' }
+      it 'responds to request for xml' do
+        get :index
+        expect(response.body).to eq(@actions.to_xml)
+      end
     end
   end
   DatabaseCleaner.clean
